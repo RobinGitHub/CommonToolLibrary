0fdedbc63d1d2bfbe3c04ffaae46330f6de5dbfc
Robin <tiankong_2010@163.com>
2015-11-25 17:59:52 +0800

    整理

---
+++ CTL/QQ截图/ImageProcessBox.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+
+namespace QQ截图
+{
+    public partial class ImageProcessBox : Control
+    {
+        #region 属性
+        #region 获取或设置用于被操作的图像
+        /// <summary>
+        /// 获取或设置用于被操作的图像
+        /// </summary>
+        private Image baseImage;
+        [EditorBrowsable(EditorBrowsableState.Always), Browsable(true), Category("Custom"), Description("获取或设置用于被操作的图像")]
+        public Image BaseImage
+        {
+            get { return baseImage; }
+            set { baseImage = value; }
+        } 
+        #endregion
+
+        #region 获取或设置操作框点的颜色
+        /// <summary>
+        /// 获取或设置操作框点的颜色
+        /// </summary>
+        private Color dotColor = Color.Yellow;
+        [EditorBrowsable(EditorBrowsableState.Always), Browsable(true), DefaultValue(typeof(Color), "Yellow"), Category("Custom"), Description("获取或设置操作框点的颜色")]
+        public Color DotColor
+        {
+            get { return dotColor; }
+            set { dotColor = value; }
+        } 
+        #endregion
+
+        #region 获取或设置操作框线条的颜色
+        /// <summary>
+        /// 获取或设置操作框线条的颜色
+        /// </summary>
+        private Color lineColor;
+        [EditorBrowsable(EditorBrowsableState.Always), Browsable(true), DefaultValue(typeof(Color), "Yellow"), Category("Custom"), Description("获取或设置操作框线条的颜色")]
+        public Color LineColor
+        {
+            get { return lineColor; }
+            set { lineColor = value; }
+        } 
+        #endregion
+
+        #region 获取当前选中的区域
+        /// <summary>
+        /// 获取当前选中的区域
+        /// </summary>
+        private Rectangle selectedRectangle;
+        /// <summary>
+        /// 获取当前选中的区域
+        /// </summary>
+        [Browsable(false)]
+        public Rectangle SelectedRectangle
+        {
+            get
+            {
+                Rectangle rectTemp = selectedRectangle;
+                rectTemp.Width++;
+                rectTemp.Height++;
+                return rectTemp;
+            }
+        } 
+        #endregion
+
+        #region 获取或设置图像放大的倍数
+        /// <summary>
+        /// 获取或设置图像放大的倍数
+        /// </summary>
+        private Size magnifySize;
+        /// <summary>
+        /// 获取或设置放大图像的原图大小尺寸
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Always), Browsable(true), DefaultValue(typeof(Size), "15,15"), Category("Custom"), Description("获取或设置放大图像的原图大小尺寸")]
+        public Size MagnifySize
+        {
+            get { return magnifySize; }
+            set
+            {
+                magnifySize = value;
+                if (magnifySize.Width < 5) magnifySize.Width = 5;
+                if (magnifySize.Width > 20) magnifySize.Width = 20;
+                if (magnifySize.Height < 5) magnifySize.Height = 5;
+                if (magnifySize.Height > 20) magnifySize.Height = 20;
+            }
+        } 
+        #endregion
+
+        #region 获取或设置图像放大的倍数
+        /// <summary>
+        /// 获取或设置图像放大的倍数
+        /// </summary>
+        private int magnifyTimes;
+        [EditorBrowsable(EditorBrowsableState.Always), Browsable(true), DefaultValue(7), Category("Custom"), Description("获取或设置图像放大的倍数")]
+        public int MagnifyTimes
+        {
+            get { return magnifyTimes; }
+            set
+            {
+                magnifyTimes = value;
+                if (magnifyTimes < 3) magnifyTimes = 3;
+                if (magnifyTimes > 10) magnifyTimes = 10;
+            }
+        } 
+        #endregion
+
+
+
+        #endregion
+
+
+        #region 构造函数
+        public ImageProcessBox()
+        {
+            InitializeComponent();
+        } 
+        #endregion
+
+
+
+    }
+}
---
+++ CTL/QQ截图/ToolButton.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms.Design;
+using System.Drawing;
+using System.Windows.Forms;
+using QQ截图.Properties;
+
+
+namespace QQ截图
+{
+    [Designer(typeof(ToolButtonDesigner))]
+    public partial class ToolButton : Control
+    {
+        #region 属性
+        private Image btnImage;
+        public Image BtnImage
+        {
+            get { return btnImage; }
+            set { btnImage = value; }
+        }
+
+        private bool isSingleSelectedBtn;
+        public bool IsSingleSelectedBtn
+        {
+            get { return isSingleSelectedBtn; }
+            set
+            {
+                isSingleSelectedBtn = value;
+                if (isSingleSelectedBtn)
+                    this.isSelectedBtn = true;
+            }
+        }
+
+        private bool isSelectedBtn;
+        public bool IsSelectedBtn
+        {
+            get { return isSelectedBtn; }
+            set
+            {
+                isSelectedBtn = value;
+                if (!isSelectedBtn)
+                    this.isSingleSelectedBtn = false;
+            }
+        }
+
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                if (value == isSelected)
+                    return;
+                isSelected = value;
+                this.Invalidate();
+            }
+        }
+
+        #endregion
+
+        #region 构造函数
+        public ToolButton()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region override
+        private bool m_bMouseEnter;
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            m_bMouseEnter = true;
+            this.Invalidate();
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            m_bMouseEnter = false;
+            this.Invalidate();
+            base.OnMouseLeave(e);
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (this.isSelectedBtn)
+            {
+                if (this.isSelected)
+                {
+                    if (!this.isSingleSelectedBtn)
+                    {
+                        this.isSelected = false;
+                        this.Invalidate();
+                    }
+                }
+                else
+                {
+                    this.isSelected = true;
+                    this.Invalidate();
+                    int len = this.Parent.Controls.Count;
+                    for (int i = 0; i < len; i++)
+                    {
+                        if (this.Parent.Controls[i] is ToolButton && this.Parent.Controls[i] != this)
+                        {
+                            if (((ToolButton)(this.Parent.Controls)[i]).isSelected)
+                            {
+                                ((ToolButton)(this.Parent.Controls[i])).IsSelected = false;
+                            }
+                        }
+                    }
+                }
+            }
+            this.Focus();
+            base.OnClick(e);
+        }
+
+        protected override void OnDoubleClick(EventArgs e)
+        {
+            this.OnClick(e);
+            base.OnDoubleClick(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (m_bMouseEnter)
+            {
+                g.FillRectangle(Brushes.LightBlue, this.ClientRectangle);
+                g.DrawRectangle(Pens.DarkCyan, new Rectangle(0, 0, this.Width - 1, this.Height - 1));
+            }
+            if (this.btnImage == null)
+            {
+                g.DrawImage(Resources.none, new Rectangle(2, 2, 17, 17));
+            }
+            else
+            {
+                g.DrawImage(this.btnImage, new Rectangle(2, 2, 17, 17));
+            }
+            g.DrawString(this.Text, this.Font, Brushes.Black, 21, (this.Height - this.Font.Height) / 2);
+            if (this.isSelected)
+            {
+                g.DrawRectangle(Pens.DarkCyan, new Rectangle(0, 0, this.Width - 1, this.Height - 1));
+            }
+            base.OnPaint(e);
+        }
+
+        protected override void SetBoundsCore(int x, int y, int width, int height, BoundsSpecified specified)
+        {
+            base.SetBoundsCore(x, y, TextRenderer.MeasureText(this.Text, this.Font).Width + 21, 21, specified);
+        }
+        #endregion
+
+    }
+
+    public class ToolButtonDesigner : ControlDesigner
+    {
+        protected override void OnPaintAdornments(System.Windows.Forms.PaintEventArgs pe)
+        {
+            Pen p = new Pen(Color.SteelBlue, 1);
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            pe.Graphics.DrawRectangle(p, 0, 0, pe.ClipRectangle.Width - 1, 20);
+            p.Dispose();
+
+            base.OnPaintAdornments(pe);
+        }
+
+        public override SelectionRules SelectionRules
+        {
+            get
+            {
+                return base.SelectionRules & ~SelectionRules.AllSizeable;
+            }
+        }
+    }
+}
