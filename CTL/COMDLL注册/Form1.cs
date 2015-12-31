14842749733cd3b3abff331f8cb8806df3115fb0
Robin <tiankong_2010@163.com>
2015-12-31 18:02:08 +0800

    整理

---
+++ CTL/COMDLL注册/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COMDLL注册
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Interop.ImageOleLib")]
+        public static extern int DllRegisterServer();//注册时用
+        [DllImport("Interop.ImageOleLib")]
+        public static extern int DllUnregisterServer();//取消注册
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey rkTest = Registry.ClassesRoot.OpenSubKey("CLSID\\{06ADA938-0FB0-4BC0-B19B-0A38AB17F182}\\");
+            if (rkTest == null)
+            {
+                //Dll没有注册，在这里调用DllRegisterServer()吧
+                int i = DllRegisterServer();
+                if (i >= 0)
+                {    //注册成功!
+                }
+                else
+                {    //注册失败
+                }
+            }
+        }
+    }
+}
---
+++ CTL/RichTextBox消息处理/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace RichTextBox消息处理
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbDisplay = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rtbSend = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbDisplay
+            // 
+            this.rtbDisplay.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rtbDisplay.Location = new System.Drawing.Point(0, 0);
+            this.rtbDisplay.Name = "rtbDisplay";
+            this.rtbDisplay.Size = new System.Drawing.Size(585, 192);
+            this.rtbDisplay.TabIndex = 0;
+            this.rtbDisplay.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rtbSend);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 192);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(585, 184);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSend);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 376);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(585, 41);
+            this.panel2.TabIndex = 1;
+            // 
+            // rtbSend
+            // 
+            this.rtbSend.AutoWordSelection = true;
+            this.rtbSend.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtbSend.EnableAutoDragDrop = true;
+            this.rtbSend.HideSelection = false;
+            this.rtbSend.Location = new System.Drawing.Point(0, 50);
+            this.rtbSend.Name = "rtbSend";
+            this.rtbSend.Size = new System.Drawing.Size(585, 134);
+            this.rtbSend.TabIndex = 0;
+            this.rtbSend.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(498, 6);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 417);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rtbDisplay);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbDisplay;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox rtbSend;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
